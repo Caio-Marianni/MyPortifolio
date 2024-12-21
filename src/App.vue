fdae32f6179cc7b8b7985aa3151d08907553031d
Caio <caiomarianni@gmail.com>
2024-12-21 08:18:27 -0300

    V 2.2.1 (Hero Structure)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
@@ -8,9 +9,9 @@ import Footer from "./components/Footer.vue";
   <div class="absolute top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
     <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] opacity-30"></div>
     <!-- Actual aplication -->
-    <div class="md:container bg-containerColorSecondary dark:bg-[#ffffff] font-sans">
+    <div class="md:container bg-containerColorSecondary font-sans">
       <Navbar />
-      <!-- Hero -->
+      <Hero />
        <!-- Projects -->
         <!-- About -->
          <!-- Contact -->
---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col items-center justify-center text-center h-[500px] p-10">
+    <h1 class="text-white text-7xl">Helping you <span class="text-orange-500">create</span> and <span class="text-orange-500">deploy</span> your ideas </h1>
+    <!-- <h1 class="text-white text-7xl">{{ $t("Hero1") }}<span class="text-orange-500">{{ $t("HeroSpan1") }}</span>{{ $t("HeroAnd") }}<span class="text-orange-500">{{ $t("HeroSpan2") }}</span>{{ $t("Hero2") }}</h1> -->
+    
+    <p class="mt-8 mb-10 text-xl text-gray-500">some type off description, pensar na merda que eu vou escrever <br> aqui ainda , pouca coisa, bobeirinhas so </p>
+    <!-- <p class="mt-8 mb-10 text-xl text-gray-500">{{ $t("HeroSub1") }}<br>{{ $t("HeroSub2") }}</p> -->
+  
+    <div class="flex gap-8">
+      <button class="rounded-full py-2 px-4 bg-orange-600 text-white"><a href="#">Let's start</a></button>
+      <button class="border rounded-full py-2 px-4 border-orange-600 text-white"><a href="#">See Projects</a></button>
+      <!-- <button class="rounded-full py-2 px-4 bg-orange-600 text-white"><a href="#">{{ $t("HeroBtn1") }}</a></button>
+      <button class="border rounded-full py-2 px-4 border-orange-600 text-white"><a href="#">{{ $t("HeroBtn2") }}</a></button> -->
+    </div>
+  </div>
+</template>
