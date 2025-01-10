249fdf62b1f6020563bdc3d0ebc379751b725ba5
Caio <caiomarianni@gmail.com>
2025-01-10 19:08:40 -0300

    v.2.4.1 (ProjectCard structure) and color changes squema

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 import AboutCard from "./components/AboutCard.vue";
+import Projects from "./components/Projects.vue";
 import Contact from "./components/Contact.vue";
 </script>
 
 <template>
   <Navbar />
   <!-- Background Effect -->
-  <div class="absolute top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-    <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] opacity-30"></div>
+  <div class="absolute top-0 z-[-2] h-screen w-full bg-background-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_60%_at_50%_0%,#000_70%,transparent_100%)] opacity-30"></div>
   </div>
   <!-- Hero Content -->
   <div class="md:container font-sans">
@@ -19,8 +20,8 @@ import Contact from "./components/Contact.vue";
     </div>
   </div>
   <!-- Projects and More -->
-  <div class="h-screen bg-neutral-800 flex items-center justify-center">
-    teste
+  <div>
+    <Projects />
   </div>
    <Contact />
   <!-- Use REVEAL como class para o effect de fade -->
---
+++ src/components/Projects.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ProjectCard from './ProjectCard.vue';
+
+</script>
+
+<template>
+  <div class="relative h-screen bg-background-950">
+    <!-- Need to change the COLOR -->
+    <div class="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,transparent_40%,rgb(17, 68, 85)_100%)]"></div>
+    <!-- Content -->
+    <div class="relative z-10">
+      <ProjectCard />
+    </div>
+  </div>
+</template>
