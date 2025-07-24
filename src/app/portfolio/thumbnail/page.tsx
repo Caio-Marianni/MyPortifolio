3333d87bc157a6e90c3a1f7cf85f6e48edf64977
Caio <caiomarianni@gmail.com>
2025-07-24 18:13:44 -0300

    comment to deploy needed

--- src/app/portfolio/thumbnail/page.tsx
+++ src/app/portfolio/thumbnail/page.tsx
@@ -4,13 +4,14 @@ import Thumbnails from "@/components/thumbnails";
 import Footer from "@/components/footer";
 import Image from "next/image";
 import Link from "next/link";
-import LanguageSwitcher from "@/components/utils/LanguageSwitcher";
-import ThemeToggle from "@/components/utils/ThemeToggle";
+// import LanguageSwitcher from "@/components/utils/LanguageSwitcher";
+// import ThemeToggle from "@/components/utils/ThemeToggle";
 
 export default function ThumbnailsPage() {
   return (
     <div className="min-h-screen text-foreground">
-      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80">
+      <p>alteraçoes em breve</p>
+      {/* <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80">
         <div className="relative container w-full flex h-14 px-4 md:px-10 items-center justify-center">
           <Link className="mr-6 flex items-center space-x-2" rel="noopener noreferrer" aria-label="Home" href="#hero">
             <Image src="/assets/LogoOrange.webp" width={40} height={40} alt="Background person" priority />
@@ -24,7 +25,7 @@ export default function ThumbnailsPage() {
       <main>
         <Thumbnails />
       </main>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
